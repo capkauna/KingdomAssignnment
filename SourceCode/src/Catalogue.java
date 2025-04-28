5544271ca1b05ff514e38976207d9f48e103d981
Elina Grumbina <355013@via.dk>
2025-04-28 15:43:46 +0200

    Update Catalogue.java
    catalogue template

--- SourceCode/src/Catalogue.java
+++ SourceCode/src/Catalogue.java
@@ -1,3 +1,28 @@
 public class Catalogue
 {
+  private static Catalogue instance;
+
+  private Catalogue() {
+
+  }
+
+  public static synchronized Catalogue getInstance() {
+    if (instance == null) {
+      instance = new Catalogue();
+    }
+    return instance;
+  }
+
+  //template
+  public synchronized void log(String message) {
+    System.out.println("[Catalogue] " + message);
+  }
+
+
+
+  //instances for:
+  // when and how much GemTransporter add to TreasureRoom;
+  //when King holds party or cancels it;
+  //when Accountants calculated the total wealth in TreasureRoom
+  //or include logs for when one or more of them are waiting to enter TreasureRoom
 }
